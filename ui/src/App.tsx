f40106bb568163648ddd43e84862d7dec9489c89
Samuel Pietra <samuelpietra@outlook.com>
2024-10-21 00:00:00 -0300

    chore: removes unused health check

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,35 +1,9 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
 import { router } from './routes'
 
-const healthCheckPing = async (
-  setMessage: React.Dispatch<React.SetStateAction<string>>,
-  setLoading: React.Dispatch<React.SetStateAction<boolean>>,
-) => {
-  try {
-    const response = await axios.get('/_health')
-    const message = response.data
-    setMessage(message)
-  } catch (error) {
-    setMessage(
-      'Unable to check health of server. Please check that the server is started and that the proxy port matches the server port',
-    )
-  }
-}
-
 function App() {
-  const [loading, setLoading] = useState(false)
-  const [healthCheckMessage, setHealthCheckMessage] = useState('Loading...')
-
-  useEffect(() => {
-    healthCheckPing(setHealthCheckMessage, setLoading)
-  }, [])
-
-  if (!loading) console.info('Health Check from server: ', healthCheckMessage)
-
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | Pokédex" />
